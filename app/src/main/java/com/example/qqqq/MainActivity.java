22783416cdc4f6910376504caef04152a48e371d
pollichups <polina.20060210@gmail.com>
2022-02-15 18:55:34 +0200

    test

---
+++ app/src/main/java/com/example/qqqq/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.qqqq;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+        int sth=10;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
